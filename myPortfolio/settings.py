66e2ca6ad1140ebbc4478810ce35c7a56abb3eb9
Engraya <ahmadyakubuaya@gmail.com>
2023-09-10 17:40:10 +0100

    Debug Config

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ft$(-+$6a0ji3=@5c5)!)x#ccbwck6v9ipah7r#bx&##xakiu2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
