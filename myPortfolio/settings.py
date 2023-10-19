c82a28d368428ff9ef72a119e0789b7a5b75d3d9
Engraya <ahmadyakubuaya@gmail.com>
2023-10-19 10:42:23 +0100

    Postgres Database Config

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = 'myPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PORTFOLIO_DATABASE',
+        'USER' : 'postgres',
+        'PASSWORD' : '28624747',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
