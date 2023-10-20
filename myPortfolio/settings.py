d7e07a295dc70b7751cf437ba9416ebcf8e6e438
Engraya <ahmadyakubuaya@gmail.com>
2023-10-20 22:55:57 +0100

    Vercel Config

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ft$(-+$6a0ji3=@5c5)!)x#ccbwck6v9ipah7r#bx&##xakiu2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,9 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 
 MEDIA_URL = '/media/'
